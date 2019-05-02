d040ffe17f3b2474146dc1bd344ec844d8e13b26
Harsh Gupta <31355653+theharshgupta@users.noreply.github.com>
2019-05-03 02:24:02 +0530

    updated clients

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 800,
         arrows: false,
         dots: false,
         pauseOnHover: false,
         responsive: [{
             breakpoint: 768,
             settings: {
-                slidesToShow: 4
+                slidesToShow: 3
             }
         }, {
             breakpoint: 520,
             settings: {
-                slidesToShow: 3
+                slidesToShow: 2
             }
         }]
     });
